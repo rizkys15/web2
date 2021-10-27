7a868bc45a638e945b24391908a4e7bd1aea451d
rizkys15 <kikuyyy15@gmail.com>
2021-10-27 23:48:42 +0700

    menambahkan file pengenal.py

---
+++ pengenal.py
@@ -0,0 +1,14 @@
+name=input("name : ")
+nickname=input("nickname : ")
+nim=input("nim : ")
+birthofplace=input("birthplace : ")
+age=input("age : ")
+address=input("address : ")
+phonenumber=input("phonenumber : ")
+
+
+print("assalamualaikum.. ")
+print("hi. let me introduce myself")
+print("my name is %s, so you can call me %s, my NIM is %s, i was born in %s, and now i'm %s years old, i live in %s amnd im very glad if you come to visit me in my house, so dont forget to contact me in %s" % (name, nickname, nim, birthofplace, age, address, phonenumber))
+print("for the last, i say, nice to meet you, and thank you for your attention")
+input("have a good day")
